676e76276ed42728709a402eab3117ed33a26d21
olegs <svytsop@gmail.com>
2024-05-07 00:44:29 +0300

    Create First V0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './assets/css/App.css';
+import Header from './components/Header'
+import HomePage from "./components/HomePage";
+import Footer  from "./components/Footer";
+import AuthForm from "./components/AuthForm";
+import RegisterForm from "./components/RegisterForm";
+import { AuthProvider } from './contexts/AuthContext';
+import Admin_Panel from "./components/Admin_Panel";
+import AboutUs from "./components/AboutUs";
+import Profile from "./components/Profile";
+import EditProfile from "./components/EditProfile";
+import PlaylistContainer from "./components/PlaylistContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <AuthProvider>
+              <div className="App">
+                  <Header />
+                  <Routes>
+                      <Route path="/" element={<HomePage />} />
+                      <Route path="/login" element={<AuthForm />} /> {/* Add a route for the login page */}
+                      <Route path='/register' element={<RegisterForm />}/>
+                      {/* eslint-disable-next-line react/jsx-pascal-case */}
+                      <Route path='/admin' element={<Admin_Panel />}/>
+                      <Route path='/about_us' element={<AboutUs />}/>
+                      <Route path='/profile' element={<Profile />}/>
+                      <Route path='/edit_profile' element={<EditProfile />} />
+                      <Route path='/userplaylist' element={<PlaylistContainer />}/>
+                  </Routes>
+                  <Footer />
+              </div>
+          </AuthProvider>
+      </Router>
   );
 }
 
